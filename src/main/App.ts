071979f8f1b696bb311827ed1a6f1bd20ad385b1
Dan Coates <mail@dancoat.es>
2020-02-09 21:55:58 +1100

    add db initialisation stuff

--- src/main/App.ts
+++ src/main/App.ts
@@ -2,17 +2,37 @@ import electron from 'electron';
 import path from 'path';
 import Database from 'better-sqlite3';
 
+const appDbTables = {
+    tabs: 'CREATE TABLE tabs (id TEXT, name TEXT, active_view TEXT, query TEXT, variables TEXT)',
+    data_tables: 'CREATE TABLE data_tables (id TEXT, table_name TEXT, file_name TEXT)'
+};
+
+
 export default class App {
-    userDataPath: string;
     static version = 1;
+
     appDb: Database.Database;
+    dataDb: Database.Database;
+
+
+
     constructor() {
-        this.userDataPath = electron.app.getPath('userData');
-        const dbPath = path.resolve(this.userDataPath, `appDb_v${App.version}.db`);
-        this.appDb = new Database(dbPath);
+        const userDataPath = electron.app.getPath('userData');
+        const appDbPath = path.resolve(userDataPath, `appDb_v${App.version}.db`);
+        const dataDbPath = path.resolve(userDataPath, `dataDb_v${App.version}.db`);
+        this.appDb = new Database(appDbPath);
+        this.dataDb = new Database(dataDbPath);
+        this.initAppDb();
+    }
 
-        const tables = this.appDb.prepare(`SELECT name FROM sqlite_master WHERE type='table';`).all();
-        console.log(tables, 'test');
+    initAppDb() {
+        const existingTables = this.appDb.prepare(`SELECT name FROM sqlite_master WHERE type='table';`)
+            .all()
+            .map(ii => ii.name);
 
+        Object.entries(appDbTables).forEach(([tableName, query]) => {
+            if(existingTables.includes(tableName)) return;
+            this.appDb.prepare(query).run();
+        });
     }
 }
